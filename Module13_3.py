e66391fb4ef69b6f1d45707d1748d5fb5f234faa
V1adimir-Ragozin <vragozin1995@gmail.com>
2024-09-15 12:42:03 +0300

    Add files via upload

---
+++ Module13_3.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+import logging
+import sys
+
+with open('Token.txt', 'r', encoding='UTF-8') as f:
+    api = f.read()
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    executor.start_polling(dp, skip_updates=True)
